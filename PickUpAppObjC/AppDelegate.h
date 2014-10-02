be5d5eb8cbe11187e945e2c54da59969812fb633
austonj <auston.jin@gmail.com>
2014-10-01 22:55:26 -0700

    Initial Commit

---
+++ PickUpAppObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickUpAppObjC
+//
+//  Created by SJ on 2014-10-01.
+//  Copyright (c) 2014 Wolfpack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickUpAppObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickUpAppObjC
+//
+//  Created by SJ on 2014-10-01.
+//  Copyright (c) 2014 Wolfpack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
