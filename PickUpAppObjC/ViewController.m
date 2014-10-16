cc93ba36bfca08c1a64ea032856c3df3b359c2b4
austonj <auston.jin@gmail.com>
2014-10-15 18:02:57 -0700

    userLogin POST request code refactored

--- PickUpAppObjC/ViewController.m
+++ PickUpAppObjC/ViewController.m
@@ -28,6 +28,7 @@
     NSInteger success = 0;
     @try {        
         
+        //
         NSString *emailStr = self.txtEmail.text;
         NSString *passwordStr = self.txtPassword.text;
         
@@ -37,32 +38,19 @@
         NSMutableDictionary *userLogin = [[NSMutableDictionary alloc] init];
         [userLogin setObject:user forKey:@"userLogin"];
         
-        
-//        NSDictionary *emailDict = [[NSDictionary alloc] initWithObjectsAndKeys:emailStr, @"email", nil];
-//        NSDictionary *passwordDict = [[NSDictionary alloc] initWithObjectsAndKeys:passwordStr, @"password", nil];
-//        NSArray *userDict = [[NSArray alloc] initWithObjects:emailDict, passwordDict, nil];
-//        NSDictionary *loginDict = [[NSDictionary alloc] initWithObjectsAndKeys:userDict, @"userLogin", nil];
-        
-        // create a JSON string from the NSDictionary
+        // create a JSONObject from the NSDictionary and an error object
         NSError *error;
         NSData *jsonData = [NSJSONSerialization dataWithJSONObject:userLogin options:NSJSONWritingPrettyPrinted error:&error];
-//        NSString *jsonString = [[NSString alloc] init];
-//        if (!jsonData) {
-//            NSLog(@"Got an error: %@", error);
-//        } else {
-//            jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-//        }
-        
         
         //Create a request
         NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
         [request setURL:[NSURL URLWithString:@"http://pickupapp.herokuapp.com/users/login"]];
         [request setHTTPMethod:@"POST"];
-        //[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
         [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
         [request setHTTPBody:jsonData];
         
-        //print out the data contents
+        //TO BE REMOVED. DEBUGGING.
+        //Print out the data contents
         NSString *jsonSummary = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
         NSLog(@"jsonSummary:%@", jsonSummary);
 
@@ -71,31 +59,6 @@
         NSData *requestHandler = [NSURLConnection sendSynchronousRequest:request returningResponse:&requestResponse error:nil];
         NSString *requestReply = [[NSString alloc] initWithBytes:[requestHandler bytes] length:[requestHandler length] encoding:NSASCIIStringEncoding];
         NSLog(@"requestReply: %@", requestReply);
-        
-//        //Create a request
-//        NSURL *url=[NSURL URLWithString:@"http://pickupapp.herokuapp.com/users/login"];
-//        NSData *postData = [jsonString dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-//        NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-//        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-//        [request setURL:url];
-//        [request setHTTPMethod:@"POST"];
-//        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-//        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-//        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-//        //[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-//        [request setHTTPBody:postData];
-//        
-//        //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
-//        
-//        //NSError *error = [[NSError alloc] init];
-//        NSHTTPURLResponse *response = nil;
-//        NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-//        
-//        NSLog(@"Response code: %ld", (long)[response statusCode]);
-
-        
-        
-        
     }
     @catch (NSException * e) {
         NSLog(@"Exception: %@", e);
